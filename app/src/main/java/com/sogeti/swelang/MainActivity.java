006793a820209d873890ca767007a5f63b476220
forestproducts\procalu <carl.lundin@sogeti.com>
2019-02-22 16:15:08 +0100

    First

---
+++ app/src/main/java/com/sogeti/swelang/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sogeti.swelang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
